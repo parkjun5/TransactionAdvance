3ee4cb8711b7b96fbda779f63cba269bfefa044d
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-11-09 22:07:39 +0900

    ReadOnlyService 외부에서 이 메소드를 불러 올 시 @Transactional(readOnly = true) 와 setCurrentTransactionReadOnly(true)로 리드온리 설정을 바꾸려 시도한다.

---
+++ src/main/java/org/example/application/ReadOnlyService.java
@@ -0,0 +1,27 @@
+package org.example.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.domain.ReadOnly;
+import org.example.domain.repository.ReadOnlyRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReadOnlyService {
+
+    private final ReadOnlyRepository readOnlyRepository;
+
+    public List<ReadOnly> findAll() {
+        TransactionSynchronizationManager.setCurrentTransactionReadOnly(true);
+        boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        log.info("ReadOnlyService method read only = {}", readOnly);
+        return readOnlyRepository.findAll();
+    }
+}
