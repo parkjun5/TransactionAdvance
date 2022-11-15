0bb786dfa6955cdca02637bca242cf61d603bf90
박세준 <sjpark@selab.co.kr>
2022-11-15 14:25:25 +0900

    트랜잭션 전파 롤백 테스트(외부 트랜잭션, 내부 트랜잭션)

--- src/test/java/org/example/propagation/BasicTxTest.java
+++ src/test/java/org/example/propagation/BasicTxTest.java
@@ -93,8 +93,23 @@ public class BasicTxTest {
         log.info("내부 트랜잭션 커밋");
         txManager.commit(inner);
 
-        log.info("외부 트랜잭션 커밋");
+        log.info("외부 트랜잭션 롤백");
         txManager.rollback(outer);
+    }
+
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); //roll-back only 표시
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
     }
 
 }
